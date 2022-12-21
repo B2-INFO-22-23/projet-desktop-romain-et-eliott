53c04bdad5a2720745f6abeaa040612d14a07bce
Eliott Guignabaudet <eliott.guignabaudet@gmail.com>
2022-12-21 18:54:31 +0100

    remove comment in connect methode

--- e-commerce-desktop/Model/ConnectionDAO.cs
+++ e-commerce-desktop/Model/ConnectionDAO.cs
@@ -30,30 +30,7 @@ namespace e_commerce_desktop.Model
             using var connection = new MySqlConnection(builder.ConnectionString);
             connection.Open();
             
-            this.connection = connection;
-
-            //tests
-            /*using var command = this.connection.CreateCommand();
-            command.CommandText = @"SELECT * FROM products";
-            MySqlDataReader reader = command.ExecuteReader();
-            List<Product> productsList = new List<Product>();
-            while (reader.Read())
-            {
-                Product product = new Product();
-
-                //var id = reader.GetValue(0);
-                product.ProductId = (int)reader.GetValue(0);
-                product.ProductName = (string)reader.GetValue(1);
-                product.Price = (float)reader.GetValue(2);
-                product.Quantity = (int)reader.GetValue(3);
-                product.Publishers = (string)reader.GetValue(4);
-                product.CategoryId= (int)reader.GetValue(5);
-                product.CreationDate= (string)reader.GetValue(6);
-                product.Description= (string)reader.GetValue(7);
-                
-                productsList.Add(product);
-            }
-            Console.WriteLine(productsList.ToArray());*/
+            this.connection = connection.Clone();
             
             return connection.Clone();
         }
