566d4138c98b6efcd94fe6415cf7abb8c6b56797
RFENZL <rfenzl@DESKTOP-TET0DOK.localdomain>
2022-12-21 19:05:19 +0100

    changement de page

---
+++ e-commerce-desktop/MainWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Controls;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
+
+namespace e_commerce_desktop
+{
+    // <summary>
+    // Interaction logic for MainWindow.xaml
+    // </summary>
+    public partial class MainWindow : Window
+    {
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = nom.Text;
+            string mdp = mot_de_passe.Text;
+            if (name == "root" && mdp == "")
+            {
+                Produit pg = new Produit(name, mdp);
+                this.Content = pg;
+            }
+            else
+            {
+              //error.Text = "Identifiants incorrects";
+            }
+        }
+
+    }
+    }
