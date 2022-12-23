71ea1c0e949c504f4ca35e8fa7dfc8d2f8021c72
RFENZL <rfenzl@DESKTOP-TET0DOK.localdomain>
2022-12-23 10:26:30 +0100

    presque affichage des produits

---
+++ e-commerce-desktop/Model/Products.cs
@@ -0,0 +1,119 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace e_commerce_desktop.Model
+{
+    internal class Products
+    {
+        //public DataTable table = new();
+
+        public List<Product> GetProducts(MySqlConnection connection)
+        {
+            MySqlConnection newConnection = connection.Clone();
+            newConnection.Open();
+            List<Product> products = new List<Product>();
+            using var command = newConnection.CreateCommand();
+            command.CommandText = @"SELECT * FROM products";
+            //MySqlDataAdapter cmd = new MySqlDataAdapter(command);
+            // cmd.Fill(table);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Product product = new();
+                product.ProductId = (int)reader.GetValue(0);
+                product.ProductName = (string)reader.GetValue(1);
+                product.Price = (float)reader.GetValue(2);
+                product.Quantity = (int)reader.GetValue(3);
+                product.Publishers = (string)reader.GetValue(4);
+                product.CategoryId = (int)reader.GetValue(5);
+                product.CreationDate = (string)reader.GetValue(6);
+                product.Description = (string)reader.GetValue(7);
+
+                products.Add(product);
+            }
+            return products;
+        }
+
+        public Product GetProductById(int id, MySqlConnection connection)
+        {
+            MySqlConnection newConnection = connection.Clone();
+            newConnection.Open();
+            Product product = new Product();
+            using var command = newConnection.CreateCommand();
+            command.CommandText = @"SELECT * FROM products WHERE ProductId=" + id + ";";
+            MySqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                product.ProductId = (int)reader.GetValue(0);
+                product.ProductName = (string)reader.GetValue(1);
+                product.Price = (float)reader.GetValue(2);
+                product.Quantity = (int)reader.GetValue(3);
+                product.Publishers = (string)reader.GetValue(4);
+                product.CategoryId = (int)reader.GetValue(5);
+                product.CreationDate = (string)reader.GetValue(6);
+                product.Description = (string)reader.GetValue(7);
+            };
+            return product;
+        }
+
+        public void AddProduct(MySqlConnection connection, string ProductName, float Price, int Quantity, string Publishers, int CategoryId, string Description)
+        {
+            MySqlConnection newConnection = connection.Clone();
+            newConnection.Open();
+            using var command = newConnection.CreateCommand();
+
+            var dateAndTime = DateTime.Now;
+            var date = dateAndTime.Date;
+            command.CommandText = "INSERT INTO products (ProductName, Price, Quantity, Publishers, CategoryId, CreationDate, Description) VALUE (@ProductName, @Price, @Quantity, @Publishers, @CategoryId, @CreationDate, @Description)";
+            command.Parameters.AddWithValue("@ProductName", ProductName);
+            command.Parameters.AddWithValue("@Price", Price);
+            command.Parameters.AddWithValue("@Quantity", Quantity);
+            command.Parameters.AddWithValue("@Publishers", Publishers);
+            command.Parameters.AddWithValue("@CategoryId", CategoryId);
+            command.Parameters.AddWithValue("@Description", Description);
+            command.Parameters.AddWithValue("@CreationDate", date);
+            command.ExecuteNonQuery();
+        }
+
+        public void UpdateProduct(MySqlConnection connection, int id, string ProductName, float Price, int Quantity, string Publishers, int CategoryId, string Description)
+        {
+            MySqlConnection newConnection = connection.Clone();
+            newConnection.Open();
+            using var command = newConnection.CreateCommand();
+
+
+            command.CommandText = @"UPDATE products SET ProductName=@ProductName, Price=@Price, Quantity=@Quantity, Publishers=@Publishers, CategoryId=@CategoryId, Description=@Description WHERE ProductId=@id;";
+
+            command.Parameters.AddWithValue("@ProductName", ProductName);
+            command.Parameters.AddWithValue("@Price", Price);
+            command.Parameters.AddWithValue("@Quantity", Quantity);
+            command.Parameters.AddWithValue("@Publishers", Publishers);
+            command.Parameters.AddWithValue("@CategoryId", CategoryId);
+            command.Parameters.AddWithValue("@Description", Description);
+            command.Parameters.AddWithValue("@id", id);
+            Console.WriteLine(command.ToString());
+            command.ExecuteNonQuery();
+        }
+
+
+        public void DeleteProduct(MySqlConnection connection, int id)
+        {
+            MySqlConnection newConnection = connection.Clone();
+            newConnection.Open();
+            using var command = newConnection.CreateCommand();
+            command.CommandText = @"SET FOREIGN_KEY_CHECKS=0;
+DELETE FROM products WHERE ProductId=" + id + ";" +
+"SET FOREIGN_KEY_CHECKS=1;";
+
+            command.ExecuteNonQuery();
+        }
+
+    }
+}
