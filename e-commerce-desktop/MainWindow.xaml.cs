a21b98eb27ffcb52f682da2dc887df530e4330aa
RFENZL <rfenzl@DESKTOP-TET0DOK.localdomain>
2022-12-23 10:19:44 +0100

    pas fini

---
+++ e-commerce-desktop/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Controls;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
+using e_commerce_desktop.Model;
+
+namespace e_commerce_desktop
+{
+    // <summary>
+    // Interaction logic for MainWindow.xaml
+    // </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ConnectionDAO connectionDAO = new ConnectionDAO();
+            var connection = connectionDAO.Connect("localhost", "root", "", "desktop");
+            if (connection != null)
+            {
+                connection.Open();
+                Products products = new Products();
+                products.GetProducts(connection);
+
+            }
+
+
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = nom.Text;
+            string mdp = mot_de_passe.Text;
+            ConnectionDAO connectionDAO = new ConnectionDAO();
+            var connection = connectionDAO.Connect("localhost", name, mdp, "desktop");
+            if (connection != null)
+            {
+                Productpage pg = new Productpage(connection);
+                this.Content = pg;
+            }
+            else
+            {
+                //error.Text = "Identifiants incorrects";
+            }
+        }
+
+    }
+}
---
+++ e-commerce-desktop/Productpage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using e_commerce_desktop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using e_commerce_desktop;
+using MySqlConnector;
+using System.Data;
+
+namespace e_commerce_desktop
+{
+    public partial class Productpage : Page
+    {
+        MySqlConnection connection;
+
+
+        public Productpage(MySqlConnection connection)
+        {
+            InitializeComponent();
+            this.connection = connection;
+            Products Productslist = new();
+            var product = Productslist.GetProducts(this.connection);
+            for (int i = 0; i < product.Count; i++)
+            {
+                produits.ItemsSource = product;
+            }
+
+        }
+    }
+}
